f66134b5f9fa6de61c9f51dd8ada9f25d0da391a
cdmuhs <cdmuhs@gmail.com>
2018-04-02 10:19:55 -0700

    cleanup

--- scripts/99-check_col_lengths.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(readxl)
-
-cr_2015 = read_csv("c://temp/gis/crashes2015/crashes2015_gdb.csv", n_max = 100)
-ncol(cr_2015)
-
-
-cr_arts = read_excel("c://temp/20180228_2011 to 2015.xlsx", sheet = "Raw 2011-2015", n_max = 100)
-
